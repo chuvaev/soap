2674c2336ad8c1ceee45381c2e69e9fe37ce9abf
Ilya_Chuvaev <ilya_chuvaev@epam.com>
2019-03-21 16:23:00 +0300

    add entity

---
+++ src/main/java/com/ilyachuvaev/webservices/Contact.java
@@ -0,0 +1,31 @@
+package com.ilyachuvaev.webservices;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.xml.bind.annotation.*;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "Contact", propOrder = {"id", "firstName", "lastName", "phone", "email"})
+public class Contact implements Serializable {
+
+    @XmlElement(name = "id", required = true)
+    private Long id;
+    @XmlElement(name = "firstName", required = true)
+    private String firstName;
+    @XmlElement(name = "lastName", required = true)
+    private String lastName;
+    @XmlElement(name = "phone", required = true)
+    private String phone;
+    @XmlElement(name = "email", required = true)
+    private String email;
+
+
+}
+
